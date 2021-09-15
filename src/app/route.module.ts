ee4177d9075c56ee3e3bdf81a39d68f1cc919336
ogo okafor <ogo.okafor@turnkeyafrica.com>
2021-09-15 12:37:43 +0100

    Add new routes

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
+import { CategoryListComponent } from "./setups/organization-setup/category/category-list/category-list.component";
+import { CategoryComponent } from "./setups/organization-setup/category/category.component";
+import { OrganizationSetupComponent } from "./setups/organization-setup/organization-setup.component";
+import { SetupComponent } from "./setups/setup.component";
 import { TemplateComponent } from "./template/template.component";
 
 const routes :Routes =[
-    {path:'',component:TemplateComponent,children:[
-        {path:'dashboard',component:DashboardComponent}
+    {path:'app',component:TemplateComponent,children:[
+        {path:'dashboard',component:DashboardComponent},
+        {path:'setups',component:SetupComponent},
+        {path:'organization-setup',component:OrganizationSetupComponent,children:[
+            {path:'categories', component:CategoryComponent}
+        ]}
     ]},
     {path:'login',component:LoginComponent},
 
