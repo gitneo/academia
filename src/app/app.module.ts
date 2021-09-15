0217a477da038f0025b77f726a92c049e7148a8a
ogo okafor <ogo.okafor@turnkeyafrica.com>
2021-09-15 12:38:46 +0100

    add new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RouteModule } from './route.module';
+import { SetupComponent } from './setups/setup.component';
 import { TemplateComponent } from './template/template.component';
+import { CategoryListComponent } from './setups/organization-setup/category/category-list/category-list.component';
+import { OrganizationSetupComponent } from './setups/organization-setup/organization-setup.component';
+import { CategoryComponent } from './setups/organization-setup/category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    TemplateComponent
+    TemplateComponent,
+    SetupComponent,
+    OrganizationSetupComponent,
+    CategoryListComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    RouteModule
+    RouteModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
