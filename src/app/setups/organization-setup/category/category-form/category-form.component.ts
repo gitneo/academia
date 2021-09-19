a1b8ec0fab04098a1c8a893bb774432fbe0292ad
ogo okafor <ogo.okafor@turnkeyafrica.com>
2021-09-19 14:38:41 +0100

    ability to save category

--- src/app/setups/organization-setup/category/category-form/category-form.component.ts
+++ src/app/setups/organization-setup/category/category-form/category-form.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormControl, FormControlName, FormGroup } from "@angular/forms";
 import { submitAction } from "src/app/shared/enum/operation.enum";
 import { ModalService } from "src/app/shared/modal/modal-service/modal.service";
+import Swal from "sweetalert2";
 import { category } from "../category-interface/category.interface";
 import { CategoryService } from "../category-service/category.service";
 
@@ -13,11 +14,13 @@ export class CategoryFormComponent implements OnInit{
     
     action :string;
     form :FormGroup;
+    @ViewChild("closeButton") closeButton: ElementRef
 
     constructor(private categoryService :CategoryService, private modalService :ModalService){}
     
     ngOnInit(): void {
         this.form = new FormGroup({
+            id:   new FormControl(),
             code : new FormControl(),
             title: new FormControl(),
             description: new FormControl()
@@ -25,21 +28,55 @@ export class CategoryFormComponent implements OnInit{
         this.modalService.buttonEventEmitter.subscribe(action=> this.action = action)
     }
 
-    onSave(type :category){console.log('create action')}
+    onSave(category :category){
+        this.categoryService.post(category).subscribe(data => {
+            if(data){
+                this.successfully('saved')
+            }else{
+                this.failed('save')
+            }
+        })
+    }
 
-    getAll(){}
+   getAll(){this.categoryService.get().subscribe(data =>this.form.setValue(data))}
 
-    onUpdate(type :category){console.log('update action')}
+    onUpdate(category :category){console.log('update action')}
 
-    onDelete(type :category){console.log('delete action')}
+    onDelete(category :category){console.log('delete action')}
 
     onSubmit(){
-        if(this.action === submitAction.CREATE){ this.onSave(this.form.value())}
-        if(this.action === submitAction.UPDATE){ this.onUpdate(this.form.value())}
-        if(this.action === submitAction.DELETE){ this.onDelete(this.form.value())}
+        console.log('this.action: ' + this.action + ' ' + submitAction.CREATE);
+        if(this.action === submitAction.CREATE){ this.onSave(this.form.value)}
+        if(this.action === submitAction.UPDATE){ this.onUpdate(this.form.value)}
+        if(this.action === submitAction.DELETE){ this.onDelete(this.form.value)}
     }
 
     clearForm(){
         this.form.reset();
+    }
+
+    successfully(operation_type :string){
+        this.getAll();
+        this.form.reset();
+        Swal.fire({  
+            icon: 'success',
+            title: 'Category successfully '+ operation_type,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+            }); 
+        this.closeButton.nativeElement.click();
+    }
+
+    failed(operation_type :string){
+        Swal.fire({  
+            icon: 'error',
+            title: 'Category failed to '+ operation_type,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+            }); 
     }
 }
--- src/app/setups/organization-setup/category/category-service/category.service.ts
+++ src/app/setups/organization-setup/category/category-service/category.service.ts
@@ -6,8 +6,16 @@ import { category } from "../category-interface/category.interface";
 export class CategoryService{
 
     constructor(private http: HttpClient){}
+    
+    post(category : category){
+       return this.http.post('http://localhost:3000/category/create', category);
+    }
 
     get(){
         return  this.http.get<category[]>('http://localhost:3000/category/all');
+    }
+
+    update(category :category){
+        return this.http.put('http://localhost:3000/category/create', category);
     }
 }
