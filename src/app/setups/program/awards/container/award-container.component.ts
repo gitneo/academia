41c8ea5a56ed7e0324227fde79ccf2190ac05f4b
ogo okafor <ogo.okafor@turnkeyafrica.com>
2021-09-30 22:45:59 +0100

    create award container

---
+++ src/app/setups/program/awards/container/award-container.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-award-container',
+    templateUrl:'award-container.component.html'
+})
+export class AwardContainerComponent{}
