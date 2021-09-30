c63c8a15e552d386e9bc1eaf1f166964c9c68193
ogo okafor <ogo.okafor@turnkeyafrica.com>
2021-09-30 22:46:30 +0100

    create program container

---
+++ src/app/setups/program/programs/container/program-container.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-program-container',
+    templateUrl:'program-container.component.html'
+})
+export class ProgramContainerComponent{}
